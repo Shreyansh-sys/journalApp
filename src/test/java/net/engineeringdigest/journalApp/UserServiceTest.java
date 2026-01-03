ce11198cff7f85ab699701d0318dcbb8f4b46125
Shreyansh-sys <srivastavashreyansh30@gmail.com>
2026-01-02 23:43:58 -0600

    Initial commit

---
+++ src/test/java/net/engineeringdigest/journalApp/UserServiceTest.java
@@ -0,0 +1,26 @@
+package net.engineeringdigest.journalApp;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUserName(){
+
+        User user = userRepository.findByUserName("Ram");
+
+        assertEquals(4, 2 + 2);
+        assertNotNull(userRepository.findByUserName("Ram"));
+        assertTrue(user.getJournalEntries().isEmpty());
+    }
+}
